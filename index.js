43077ffbcfa5b08adda0f4225d3b74b537b8daea
BRS-2002 <89071262+BRS-2002@users.noreply.github.com>
2021-08-17 18:49:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function makered()
+{
+    document.body.style.backgroundColor="red";
+}
+function makeblue()
+{
+    document.body.style.backgroundColor="blue";
+}
+function makegreen()
+{
+    document.body.style.backgroundColor="green";
+}
+function makeyellow()
+{
+    document.body.style.backgroundColor="yellow";
+}
